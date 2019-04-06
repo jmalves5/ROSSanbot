0c64b5cce2d9e71aee64af6a6645d1b8e55a5273
jmalves5 <jmalves5@hotmail.com>
2019-04-06 23:01:55 +0100

    added camera tutorial code

--- app/src/main/java/com/example/rossanbot/MainActivity.java
+++ app/src/main/java/com/example/rossanbot/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.rossanbot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.ros.android.RosActivity;
+import org.ros.android.view.camera.RosCameraPreviewView;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMainExecutor;
+
+import java.io.IOException;
+
+public class MainActivity extends RosActivity {
+
+    private int cameraId;
+    private RosCameraPreviewView rosCameraPreviewView;
+
+    public MainActivity() {
+        super("CameraTutorial", "CameraTutorial");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.main);
+        rosCameraPreviewView = (RosCameraPreviewView) findViewById(R.id.ros_camera_preview_view);
+    }
+
+    
+
+    @Override
+    protected void init(NodeMainExecutor nodeMainExecutor) {
+        cameraId = 0;
+
+        rosCameraPreviewView.setCamera(getCamera());
+        try {
+            java.net.Socket socket = new java.net.Socket(getMasterUri().getHost(), getMasterUri().getPort());
+            java.net.InetAddress local_network_address = socket.getLocalAddress();
+            socket.close();
+            NodeConfiguration nodeConfiguration =
+                    NodeConfiguration.newPublic(local_network_address.getHostAddress(), getMasterUri());
+            nodeMainExecutor.execute(rosCameraPreviewView, nodeConfiguration);
+        } catch (IOException e) {
+            // Socket problem
+            Log.e("Camera Tutorial", "socket error trying to get networking information from the master uri");
+        }
+
+    }
+
+    private Camera getCamera() {
+        Camera cam = Camera.open(cameraId);
+        Camera.Parameters camParams = cam.getParameters();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            if (camParams.getSupportedFocusModes().contains(
+                    Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO)) {
+                camParams.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+            } else {
+                camParams.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            }
+        }
+        cam.setParameters(camParams);
+        return cam;
     }
 }
